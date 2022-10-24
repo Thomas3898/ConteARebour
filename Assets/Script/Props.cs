325a7c5e9c70d61f76f1ab8eb697a6a8c4c6592f
leonardV32 <83813949+leonardV32@users.noreply.github.com>
2022-10-24 20:05:09 +0200

    Setup Dog Scene and Props script

---
+++ Assets/Script/Props.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Props : MonoBehaviour
+{
+    Color m_HoverColor = Color.red;
+    Color m_OriginalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
