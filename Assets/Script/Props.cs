0f3d7e2006fde1f833824e835f0abf80ba746a4e
leonardV32 <83813949+leonardV32@users.noreply.github.com>
2022-10-25 19:42:19 +0200

    Highlight_Props working

--- Assets/Script/Props.cs
+++ Assets/Script/Props.cs
@@ -6,16 +6,27 @@ public class Props : MonoBehaviour
 {
     Color m_HoverColor = Color.red;
     Color m_OriginalColor;
-
+    MeshRenderer m_Renderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_Renderer.material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseOver()
+    {
+        m_Renderer.material.color = m_HoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        m_Renderer.material.color = m_OriginalColor;
     }
 }
