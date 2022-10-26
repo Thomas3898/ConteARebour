e9c84f65ea9cde4bec9712d7008fcee50a8265df
Thomas3898 <grandin.thoma@gmail.com>
2022-10-26 15:52:34 +0200

    HiddingPlaceSpawnAndMove

--- Assets/Script/Map_Manager.cs
+++ Assets/Script/Map_Manager.cs
@@ -7,11 +7,15 @@ public class Map_Manager : MonoBehaviour
     public GameManager manager;
     public GameObject[] spawnPoints;
     public GameObject[] prefabHidding;
+    public int indexSpawnPoints0;
+    public int indexSpawnPoints1;
+    public int indexSpawnPoints2;
+    public int indexSide;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnAndSweepHidding());
     }
 
     // Update is called once per frame
@@ -20,5 +24,21 @@ public class Map_Manager : MonoBehaviour
         
     }
 
+    IEnumerator SpawnAndSweepHidding()
+    {
 
+        indexSpawnPoints0 = Random.Range(0, 6);
+        indexSpawnPoints1 = Random.Range(0, 6);
+        indexSpawnPoints2 = Random.Range(0, 6);
+
+        for(int i = 0; i < 3; i++)
+        {
+            Instantiate(prefabHidding[Random.Range(0, 2)], spawnPoints[Random.Range(0, 6)].transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(0, 2));
+        }
+
+        yield return new WaitForSeconds(Random.Range(1,3));
+
+        StartCoroutine(SpawnAndSweepHidding());
+    }
 }
