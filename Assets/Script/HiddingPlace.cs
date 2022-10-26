ba02b9f6b18e281d820f5bd4cab0c8eac5b1cf84
Thomas3898 <grandin.thoma@gmail.com>
2022-10-26 15:59:37 +0200

    Setup Speed

--- Assets/Script/HiddingPlace.cs
+++ Assets/Script/HiddingPlace.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HiddingPlace : MonoBehaviour
 {
-    public float speed = -0.001f;
+    public float speed = -10f;
     // Start is called before the first frame update
     void Start()
     {
