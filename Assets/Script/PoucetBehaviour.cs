7895659ee0598b50794079639a0745e7e2b07c29
leonardV32 <83813949+leonardV32@users.noreply.github.com>
2022-12-01 17:07:09 +0100

    Game Event In
    Implementation GameEvent

--- Assets/Script/PoucetBehaviour.cs
+++ Assets/Script/PoucetBehaviour.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PoucetBehaviour : MonoBehaviour
-{   
-    // déclencher le retournement
+{
+    public UnityEvent<bool> OnTurningEnded;
+
+    // declencher le retournement
     // check si dog est actif
     // attendre la fin de l'anim
     //avertir le bush de relacher le chien
